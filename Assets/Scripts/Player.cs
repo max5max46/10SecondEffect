be6edaddda7c52cf8cbc5f8feaf570c34dbca317
Dylan Adams <dylan.adams.2016@outlook.com>
2023-10-10 14:10:41 -0300

    added a ball that can move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] private float acceleration = 2;
+    [SerializeField] private float maxSpeed = 1;
+    [SerializeField] private float deceleration = 1;
+
+    private Rigidbody rb;
+
+    private bool upPressed = false;
+    private bool downPressed = false;
+    private bool rightPressed = false;
+    private bool leftPressed = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //grabs player input and stores it
+        if (Input.GetKey(KeyCode.W))
+            upPressed = true;
+
+        if (Input.GetKey(KeyCode.S))
+            downPressed = true;
+
+        if (Input.GetKey(KeyCode.D))
+            rightPressed = true;
+
+        if (Input.GetKey(KeyCode.A))
+            leftPressed = true;
+    }
+
+    private void FixedUpdate()
+    {
+        //Force Adding Variables to zero for the next add force
+        float moveForwardBackward = 0;
+        float moveLeftRight = 0;
+
+        //Turns WASD Input into the Add Force Variables 
+        if (upPressed)
+        {
+            moveForwardBackward += 1;
+            upPressed = false;
+        }
+        if (downPressed)
+        {
+            moveForwardBackward -= 1;
+            downPressed = false;
+        }
+        if (rightPressed)
+        {
+            moveLeftRight += 1;
+            rightPressed = false;
+        }
+        if (leftPressed)
+        {
+            moveLeftRight -= 1;
+            leftPressed = false;
+        }
+
+        //Uses the Add force Variables to Add force as well as capping the speed
+        if (Math.Sqrt(Math.Pow(rb.velocity.x, 2) + Math.Pow(rb.velocity.z, 2)) < maxSpeed)
+            rb.AddForce(new Vector3(moveLeftRight, 0, moveForwardBackward).normalized * Time.deltaTime * acceleration * 1000);
+
+        //Increases deceleration to prevent sliding
+        rb.velocity = new Vector3(rb.velocity.x * (1 - (0.1f * deceleration)), rb.velocity.y, rb.velocity.z * (1 - (0.1f * deceleration)));
     }
 }
